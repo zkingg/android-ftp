5eff687fd7eb6da2111597e45910bd549b4dd675
luffyzbest <luffyzbest@gmail.com>
2013-06-14 20:25:59 +0000

    ajout de preferences

--- ServeurFTP/src/com/example/serveurftp/MainActivity.java
+++ ServeurFTP/src/com/example/serveurftp/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.serveurftp;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.io.CopyStreamListener;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -23,6 +26,18 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		Log.i("",R.id.action_settings+"="+item.getItemId());
+		switch(item.getItemId()){
+		case R.id.action_settings:
+			startActivity(new Intent(this,Preferences.class));
+			break;
+		}
 		return true;
 	}
 

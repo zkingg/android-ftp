f5be69c2e859f851fba1930593bfebc7117df411
luffyzbest <luffyzbest@gmail.com>
2013-06-17 21:13:27 +0000

    action faite: TYPE SYST
    la plupart des commande de base ont été préparé

--- ServeurFTP/src/com/ftp/core/Session.java
+++ ServeurFTP/src/com/ftp/core/Session.java
@@ -6,6 +6,13 @@ package com.ftp.core;
 public class Session {
 	private String ip;
 	private String login;
+	private char TYPE_DATA = 'I';//par défaut mode tranfert : Binaire
+	//A - ASCII text 
+	//E - EBCDIC text 
+	//I - image (binary data) 
+	//L - local format
+	
+	private boolean is_logged = false;
 	
 	public Session(){}
 	
@@ -14,10 +21,14 @@ public class Session {
 		this.login = login;
 	}
 	
+	public void setIsLogged(boolean is_logged){this.is_logged = is_logged;}
+	public boolean isLogged(){return this.is_logged;}
 	public String getIp() {return ip;}
 	public void setIp(String ip) {this.ip = ip;	}
 	public String getLogin() {return login;}
 	public void setLogin(String login) {this.login = login;}
+	public void setTypeData(char type){this.TYPE_DATA = type;}
+	public char getTypeData(){return this.TYPE_DATA;}
 	
 	public boolean connection(String mdp){
 		if(mdp.equals("mdp")){
@@ -26,5 +37,7 @@ public class Session {
 			return false;
 		}
 	}
+	
+	
 	
 }

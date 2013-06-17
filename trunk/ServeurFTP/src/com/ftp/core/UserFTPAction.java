1dd33cac717eb2477434443925d2f7e3fb4e950d
luffyzbest@gmail.com <luffyzbest@gmail.com@aa67bf7b-eb17-9e80-4c7f-d4c7b9842b19>
2013-06-17 21:13:27 +0000

    action faite: TYPE SYST
    la plupart des commande de base ont été préparé

--- trunk/ServeurFTP/src/com/ftp/core/UserFTPAction.java
+++ trunk/ServeurFTP/src/com/ftp/core/UserFTPAction.java
@@ -11,6 +11,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
+import android.os.Build;
 import android.util.Log;
 
 /**
@@ -19,19 +20,12 @@ import android.util.Log;
 public class UserFTPAction extends Thread {
 	private Socket client_socket;
 	private ServerFTP server;
-	/*private DataInputStream din;
-	private DataOutputStream dout;*/
-	/*private BufferedReader reader;
-	private PrintWriter writer;*/
+	private Session session;
 	
 	public UserFTPAction(ServerFTP server, Socket socket){
 		this.client_socket = socket;
 		this.server = server;
-
-		/*this.din = new DataInputStream(socket.getInputStream());
-		this.dout = new DataOutputStream(socket.getOutputStream());*/
-		/*reader = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-		writer = new PrintWriter(new OutputStreamWriter(client_socket.getOutputStream()), true);*/
+		
 		reply(220, "Bienvenue sur le server FTP");
 		this.start();
 
@@ -41,25 +35,21 @@ public class UserFTPAction extends Thread {
 	public void run(){
 		boolean exit = false;
 		while(!exit && client_socket.isConnected()){
-			
 			try {
-				//Thread.sleep(2000);//~debug a virer
 				String command = reception();
 				String[] args = command.split(" ");
 				Log.i("ftp-client","client "+client_socket.getInetAddress().getHostAddress()+" command :"+command);
 				
-				if(args[0].equals("LIST")){//ACTION : LIST
-					
-				}else if(args[0].equals("USER")){//ACTION : USER
-					Session session =  new Session(args[1],client_socket.getInetAddress().getHostAddress());		
+				if(args[0].equals("USER")){//ACTION : USER : connection
+					session = new Session(args[1],client_socket.getInetAddress().getHostAddress());		
 					
 					if(! this.server.anonymousConnectionAllowed()){//si need mdp
 						reply(331, "Password needed");
 						args = reception().split(" ");
-						Log.e("",args[0]);
 						if(args[0].equals("PASS")){	
 							if(session.connection(args[1])){//si conexnion ok
 								reply(230, "User connected");
+								session.setIsLogged(true);
 								this.server.addUserSession(session);
 							}else{
 								reply(530, "Mot de passe incorrect");
@@ -71,12 +61,51 @@ public class UserFTPAction extends Thread {
 							continue;
 						}
 					}else{
-						reply(331, "Anonymous connection allowed");
+						reply(230, "Anonymous connection allowed");
+						session.setIsLogged(true);
 						this.server.addUserSession(session);
 					}
 					
-				}else if(args[0].equals("QUIT")){//ACTION : EXIT
+				}else if(args[0].equals("QUIT")){//ACTION : EXIT : fermeture socket
 					exit = true;
+					
+				}else if(args[0].equals("SYST")){//ACTION : SYST : Info systeme
+					reply(215, " Android "+Build.DISPLAY);
+					
+				}else if(args[0].equals("TYPE")){//ACTION : TYPE : precise le format des données qui seront envoyé
+					session.setTypeData(args[1].charAt(0));
+					reply(200,"Type set to "+args[1].charAt(0));
+					
+				}else if(args[0].equals("PASV")){//ACTION : PASV : déclenche mode passif
+					String ip = client_socket.getInetAddress().getHostAddress();
+					String[] inet_num = ip.split("\\.");
+					int port = client_socket.getPort();
+					reply(227,"Entering Passive Mode ("+inet_num[0]+","+inet_num[1]+","+inet_num[2]+","+inet_num[3]+","+((int)port/256)+","+port%256+").");
+				
+				/** File explorer commands **/	
+				}else if(args[0].equals("PWD")){//ACTION : PWD : Retourne chemin courant
+					//pas fini ...
+					reply(257,"'\\' is current directory.");
+					
+				}else if(args[0].equals("LIST")){//ACTION : LIST : renvoye la liste des fichiers et répertoires présents dans le répertoire courant
+					reply(150,"Transfert in progress");
+					//recup list dossier
+					reply(226,"Transfert complete");
+					
+				}else if(args[0].equals("ABOR")){//ACTION : ABOR : Interuption d'un telechargement 
+				}else if(args[0].equals("DELE")){//ACTION : DELE : supprime un fichier
+				}else if(args[0].equals("RMD")){//ACTION : RMD : supprime un repertoire
+				}else if(args[0].equals("MKD")){//ACTION : MKD : créer un répertoire
+				}else if(args[0].equals("RETR")){//ACTION : RETR : Recuperation du fichier passer en paramétre
+				}else if(args[0].equals("STOR")){//ACTION : STOR : Stockage du fichier donné, écrase si déjà exitant
+				}else if(args[0].equals("STOU")){//ACTION : STOU : Stockage du fichier donné, nom unique généré, nom retourné dans reponse
+				}else if(args[0].equals("")){//si aucune action -- connexion interompue ??
+					Thread.sleep(3000);
+					exit = true;
+					
+				}
+				else{//ACTION UNDEFINED 
+					reply(500, "Command Undefined :"+args[0]);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -103,7 +132,7 @@ public class UserFTPAction extends Thread {
 		try {
 			String rep= new BufferedReader(new InputStreamReader(client_socket.getInputStream())).readLine();
 			System.out.println("réponse :"+rep);
-			return rep;
+			return rep!=null? rep : "";
 		}
 		catch (SocketTimeoutException e) {
 			return null;
